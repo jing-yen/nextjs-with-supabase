bfa04675c240794551689cbfd84390332bc478e7
Jing Yen <tong414782@gmail.com>
2024-05-12 02:26:18 +0800

    first step (tailwind->semantic, added some supabase database)
    i installed semantic ui (but havent used it), also added₫some supabase database code.

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,23 +19,14 @@ export default function Header() {
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
+        Welcome to {" "}
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
+          NUSAG
         </a>
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
